31b79e37eb303d1d90059a61700176f1cd818cd5
raejun92 <cvcv1324@naver.com>
2021-10-12 14:49:35 +0900

    exam thread_join

--- main.c
+++ main.c
@@ -13,41 +13,34 @@ pthread_mutex_destroy, pthread_mutex_lock,
 pthread_mutex_unlock
  */
 
-void p_function(void *data)
+void *t_function(void *param)
 {
-	pthread_t tid;
-
-	tid = pthread_self();
-
-	char *thread_name = (char *)data;
-	int i = 0;
-
-	while (i < 3) {
-		printf("thread name: %s, tid: %x\n", thread_name, (unsigned int)tid);
-		i++;
+	printf("쓰레드 함수 실행\n");
+	for (int i = 0; i <= 5; i++)
+	{
 		usleep(1000 * 1000);
+		printf("쓰레드 함수 실행 중...(%d/5)\n", i);
 	}
+	printf("쓰레드 함수 종료\n");
+	return (void *)2147483647;
 }
 
 int main(int argc, char **argv)
 {
 	// if (argc != 5 || argc != 6)
 	// 	exit(0);
-	pthread_t pthread;
+	pthread_t p_thread;
 	int thr_id;
-	int status;
-	char p1[] = "thread_created";
-	char p2[] = "thread_main";
+	int result;
 
-	thr_id = pthread_create(&pthread, NULL, p_function, (void *)p1);
-	if (thr_id < 0) {
-		perror("thread0 create error");
-		exit(EXIT_FAILURE);
+	thr_id = pthread_create(&p_thread, NULL, t_function, NULL);
+	if (thr_id < 0)
+	{
+		perror("thread create error:;");
+		exit(0);
 	}
-
-	p_function((void *)p2);
-
-	printf("created thread id: %x\n", pthread);
-	printf("end\n");
+	pthread_join(p_thread, (void *)&result);
+	printf("thread join 실행됨:%d\n", result);
+	printf("main() 종료\n");
 	return (0);
 }
