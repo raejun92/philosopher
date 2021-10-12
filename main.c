6f949e08da743afcade1556494eab7030484ea82
raejun92 <cvcv1324@naver.com>
2021-10-12 17:14:29 +0900

    exam mutex

--- main.c
+++ main.c
@@ -11,36 +11,46 @@ usleep, gettimeofday, pthread_create,
 pthread_detach, pthread_join, pthread_mutex_init,
 pthread_mutex_destroy, pthread_mutex_lock,
 pthread_mutex_unlock
- */
 
-void *t_function(void *param)
+식사를 마치면 포크를 내려놓고 잠을 잔다
+잠을 다 자면 생각한다
+생각 후 식사한다
+ */
+pthread_mutex_t mutex;
+int cnt = 0;
+
+void *count(void *arg)
 {
-	printf("쓰레드 함수 실행\n");
-	for (int i = 0; i <= 5; i++)
+	int i;
+	char *name = (char *)arg;
+
+	pthread_mutex_lock(&mutex);
+
+	cnt = 0;
+	for (i = 0; i < 10; i++)
 	{
-		usleep(1000 * 1000);
-		printf("쓰레드 함수 실행 중...(%d/5)\n", i);
+		printf("%s cnt: %d\n", name, cnt);
+		cnt++;
+		usleep(1);
 	}
-	printf("쓰레드 함수 종료\n");
-	return (void *)2147483647;
+	
+	pthread_mutex_unlock(&mutex);
 }
 
 int main(int argc, char **argv)
 {
 	// if (argc != 5 || argc != 6)
 	// 	exit(0);
-	pthread_t p_thread;
-	int thr_id;
-	int result;
+	pthread_t thread1, thread2;
 
-	thr_id = pthread_create(&p_thread, NULL, t_function, NULL);
-	if (thr_id < 0)
-	{
-		perror("thread create error:;");
-		exit(0);
-	}
-	pthread_join(p_thread, (void *)&result);
-	printf("thread join 실행됨:%d\n", result);
-	printf("main() 종료\n");
+	pthread_mutex_init(&mutex, NULL);
+	
+	pthread_create(&thread1, NULL, count, (void *)"thread1");
+	pthread_create(&thread2, NULL, count, (void *)"thread2");
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
