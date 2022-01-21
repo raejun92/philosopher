d317b71d00b282963c2183e67acdd1c8f2a9e7c0
raejun92 <cvcv1324@naver.com>
2022-01-21 13:36:04 +0900

    fix: 출력 변수로 간소화

--- ft_util2.c
+++ ft_util2.c
@@ -6,15 +6,15 @@ int	send_message(t_philo *philo, int status)
 
 	output_time = get_current_time() - philo->data->start_time;
 	if (philo->data->check_death == 0 && status == 1)
-		printf("%lu %d has taken a fork\n", get_current_time() - philo->data->start_time, philo->name);
+		printf("%lu %d has taken a fork\n", output_time, philo->name);
 	else if (philo->data->check_death == 0 && status == 2)
-		printf("%lu %d is eating\n", get_current_time() - philo->data->start_time, philo->name);
+		printf("%lu %d is eating\n", output_time, philo->name);
 	else if (philo->data->check_death == 0 && status == 3)
-		printf("%lu %d is sleeping\n", get_current_time() - philo->data->start_time, philo->name);
+		printf("%lu %d is sleeping\n", output_time, philo->name);
 	else if (philo->data->check_death == 0 && status == 4)
-		printf("%lu %d is thinking\n", get_current_time() - philo->data->start_time, philo->name);
+		printf("%lu %d is thinking\n", output_time, philo->name);
 	else if (philo->data->check_death == 1 && status == 0)
-		printf("%lu %d died\n", get_current_time() - philo->data->start_time, philo->name);
+		printf("%lu %d died\n", output_time, philo->name);
 	if (philo->data->check_death > 0)
 		return (1);
 	return (0);
